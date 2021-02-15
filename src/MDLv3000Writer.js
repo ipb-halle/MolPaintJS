e2f81a45b0b491bd80b46916ec68690a25cfb735
Frank Broda <fbroda@ipb-halle.de>
2021-02-15 17:01:48 +0100

    MDLv3000Writer (still incomplete)

--- src/MDLv3000Writer.js
+++ src/MDLv3000Writer.js
@@ -18,57 +18,73 @@
 
 function MDLv3000Writer() {
 
+    this.output = "";
     this.write = function (mol) {
-
-        var st = "";
 
         mol.reIndex();
 
-        st += mol.getProperty("NAME") + "\n";
-        st += mol.getProperty("HEADER2") + "\n";
-        st += mol.getProperty("COMMENT") + "\n";
+        this.output = mol.getProperty("NAME") + "\n";
+        this.output += mol.getProperty("HEADER2") + "\n";
+        this.output += mol.getProperty("COMMENT") + "\n";
         //
         //     aaabbblllfffcccsssxxxrrrpppiii999
         //
-        st += "  0  0  0  0  0  0            999 V3000\n";
-        st += this.writeCTAB(mol);
-        st += "M  END\n";
-        return st;
+        this.output += "  0  0  0  0  0  0            999 V3000\n";
+        this.writeCTAB(mol);
+        this.output += "M  END\n";
+        return this.output;
+    }
+
+    /**
+     * check if the concatenation of line and st would exceed the 80 characters 
+     * per line limit of MDL v3000 files and make a line continuation
+     * @param line the current line containing data
+     * @param st additional data for the current line
+     * @return either concatenation (if combined length is smaller than 79) or st
+     */
+    this.extendLine = function(line, st) {
+        var lenline = line.length;
+        if ((line.length + st.length) > 78) {
+            this.output += line + '-';
+            return st;
+        }
+        return line + st;
     }
 
     this.writeCTAB = function(mol) {
-        var st = 'M  V30 BEGIN CTAB\n'
-            + this.writeCountsLine(mol);
+        this.output += 'M  V30 BEGIN CTAB\n';
+
+        this.writeCountsLine(mol);
 
         if (mol.getAtomCount() > 0) {
-            st += this.writeAtomBlock(mol);
+            this.writeAtomBlock(mol);
         }
 
         if (mol.getBondCount() > 0) {
-            st += this.writeBondBlock(mol);
+            this.writeBondBlock(mol);
         }
 
         // SGroup
 
-        return st + 'M  V30 END CTAB\n';
+        this.output += 'M  V30 END CTAB\n';
     }
 
     this.writeCountsLine = function (mol) {
 
         // atomCount, bondCount, SgroupCount, 3D-ConstraintCount, chiralFlag(0,1), [registration number]
 
-        var st = 'M  V30 COUNTS '
+        this.output += 'M  V30 COUNTS '
             + mol.getAtomCount() + ' '
             + mol.getBondCount() + ' '
             + '0 '
             + '0 '
             + '0\n';
-        return st; 
     }
 
     this.writeAtomBlock = function (mol) {
-        var st = "M  V30 BEGIN ATOM\n";
+        this.output += "M  V30 BEGIN ATOM\n";
         for (var i in mol.getAtoms()) {
+            var st = "";
             var a = mol.getAtom(i);
             //  
             //	coordinates must be flipped on X-axis!
@@ -81,12 +97,49 @@ function MDLv3000Writer() {
                     a.coordZ,
                     0                           // aamap
                 );
-            st += '\n';
+            st = this.extendLine(st, this.writeAtomCharge(a));
+            st = this.extendLine(st, this.writeAtomMass(a));
+            this.output += st + '\n';
         }
-        return st + "M  V30 END ATOM\n";
+        this.output += "M  V30 END ATOM\n";
+    }
+
+    this.writeAtomCharge = function(atom) {
+        if (atom.getCharge() != 0) {
+            return " CHG=" + atom.getCharge();
+        } 
+        return '';
+    }
+
+    this.writeAtomMass = function(atom) {
+        var iso = atom.getType().getIsotope().getIsotope();
+        if (iso != 0) {
+            return " MASS=" + iso;
+        } 
+        return '';
     }
 
     this.writeBondBlock = function (mol) {
-        return "";
+        this.output += "M  V30 BEGIN BOND\n";
+        for (var i in mol.getBonds()) {
+            var st = "";
+            var b = mol.getBond(i);
+            st += sprintf("M  V30 %d %d %d %d",
+                b.getIndex(),
+                b.getType(),
+                b.getAtomA().getIndex(),
+                b.getAtomB().getIndex());
+
+            st = this.extendLine(st, this.writeBondStereo(b));
+            this.output += st + '\n';
+        }
+        this.output += "M  V30 END BOND\n";
+    }
+
+    this.writeBondStereo = function (bond) {
+        if (bond.getStereo('v3') != 0) {
+            return " CFG=" + bond.getStereo('v3');
+        }
+        return '';
     }
 }

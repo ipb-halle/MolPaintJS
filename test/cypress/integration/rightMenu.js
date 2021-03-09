b96aa61cd1c5e79c50b68587b8b62ca46e07cff5
Frank Broda <fbroda@ipb-halle.de>
2021-03-09 21:00:44 +0100

    Additional E2E tests

--- test/cypress/integration/rightMenu.js
+++ test/cypress/integration/rightMenu.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-describe('test of the right menu (elements, isotopes, radicals)', () => {
+describe('test mostly the right menu (elements, charges, isotopes, radicals)', () => {
 
     it('tests hydrogen count on single N and O atoms', () => {
 
@@ -64,7 +64,7 @@ describe('test of the right menu (elements, isotopes, radicals)', () => {
         });
     });
 
-    it('tests charges of atom mass / isotope', () => {
+    it('tests changes of atom mass / isotope', () => {
         cy.visit('/');
         cy.wait(200);
 
@@ -74,6 +74,11 @@ describe('test of the right menu (elements, isotopes, radicals)', () => {
         cy.get('#mol_pseMenu').invoke('hide');
         cy.get('#mol_canvas').click(100, 100);
         cy.window().then((win) => {
+
+            cy.get("#mol_isotope").invoke('attr', 'src').then((src) => {
+                    expect(src).to.equal(win.molPaintJS.Resources['isotope_up.png']);
+            });
+
             var mol = win.molPaintJS.getContext('mol').getMolecule();
             expect(mol.getAtom("Atom1").getType().getIsotope().getMassExact()).to.be.closeTo(35.45, 0.002);
         });
@@ -94,8 +99,56 @@ describe('test of the right menu (elements, isotopes, radicals)', () => {
         cy.get('#mol_canvas').click(100, 100);
 
         cy.window().then((win) => {
+
+            cy.get("#mol_isotope").invoke('attr', 'src').then((src) => {
+                    expect(src).to.equal(win.molPaintJS.Resources['isotope_down.png']);
+            });
+
             var mol = win.molPaintJS.getContext('mol').getMolecule();
             expect(mol.getAtom("Atom1").getType().getIsotope().getMassExact()).to.be.closeTo(34.96885268, 0.00001);
         });
     });
+
+    it('tests change of electronic state (singlet, doublet, triplet - chemically invalid) ', () => {
+        cy.visit('/');
+        cy.wait(200);
+
+        cy.get('#mol_clear').click();
+        cy.get('#mol_carbon').click();
+        cy.get('#mol_canvas').click(100,100);
+
+        // check icon
+        cy.window().then((win) => {
+            cy.get("#mol_radical").invoke('attr', 'src').then((src) => {
+                expect(src).to.equal(win.molPaintJS.Resources['radical.png']);
+            });
+        });
+
+        for (const cfg of [
+                {"control":"singlet", "outcome":/ RAD=1/},
+                {"control":"doublet", "outcome":/ RAD=2/},
+                {"control":"triplet", "outcome":/ RAD=3/},
+                {"control":"no_radical", "outcome": /^(?!.*RAD=).*$/} ]) {
+
+            cy.get('#mol_radicalMenu').should('be.hidden').invoke('show');
+            cy.get('#mol_' + cfg.control).click();
+            cy.get('#mol_radicalMenu').invoke('hide');
+            cy.get('#mol_canvas').click(100, 100);
+            cy.window().then((win) => {
+
+                // check icon
+                cy.get("#mol_radical").invoke('attr', 'src').then((src) => {
+                    expect(src).to.equal(win.molPaintJS.Resources[cfg.control + '.png']);
+                });
+
+                var mdl = win.molPaintJS.getMDLv3000('mol').replace(/\n/g, "\\n");
+                expect(mdl).to.match(cfg.outcome);
+            });
+        }
+
+    });
+
+
+
+
 })

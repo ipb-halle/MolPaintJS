c989b8e8b2db0aff664060c9e28beec941d59e39
Frank Broda <fbroda@ipb-halle.de>
2024-03-30 16:21:31 +0100

    add history to charge tools

--- src/IsotopeTool.js
+++ src/IsotopeTool.js
@@ -1,19 +1,19 @@
 /*
  * MolPaintJS
- * Copyright 2017 Leibniz-Institut f. Pflanzenbiochemie 
- *  
+ * Copyright 2017 - 2024 Leibniz-Institut f. Pflanzenbiochemie
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *  
+ *
  */
 var molPaintJS = (function (molpaintjs) {
     "use strict";
@@ -44,8 +44,8 @@ var molPaintJS = (function (molpaintjs) {
                     this.context.getDrawing().replaceAtom(atom);
                     actionList.addAction(molPaintJS.Action("UPD", "ATOM", atom, oldAtom));
                     this.context.getHistory().appendAction(actionList);
+                    this.context.draw();
                 }
-                this.context.draw();
             },
 
             onMouseDown : function (x, y, evt) {
